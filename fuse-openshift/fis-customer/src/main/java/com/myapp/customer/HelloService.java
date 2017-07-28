bf5eeb69a2b574ace7b3f4e6359b19b9df81ae9c
Steve <stnguyen@redhat.com>
2017-07-29 00:21:49 +0800

    rest api

---
+++ fuse-openshift/fis-customer/src/main/java/com/myapp/customer/HelloService.java
@@ -0,0 +1,25 @@
+package com.myapp.customer;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.springframework.stereotype.Service;
+ 
+@Path("/sayHello")
+@Service
+public interface HelloService {
+ 
+    @GET
+    @Path("")
+    @Produces(MediaType.TEXT_PLAIN)
+    String welcome();
+    
+    @GET
+    @Path("/{a}")
+    @Produces(MediaType.TEXT_PLAIN)
+    String sayHello(@PathParam("a") String a);
+
+}
