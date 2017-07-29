d17b6098ea7d3842d5eb785d1323c38957bb296a
Steve <stnguyen@redhat.com>
2017-07-29 12:15:57 +0800

    rest api

--- fuse-openshift/fis-customer/src/main/java/com/myapp/customer/HelloServiceImpl.java
+++ fuse-openshift/fis-customer/src/main/java/com/myapp/customer/HelloServiceImpl.java
@@ -1,1 +1,16 @@
-HelloService
+package com.myapp.customer;
+
+import io.swagger.annotations.Api;
+
+@Api("/sayHello")
+public class HelloServiceImpl implements HelloService {
+
+    public String welcome() {
+        return "Welcome to the CXF RS Spring Boot application, append /{name} to call the hello service";
+    }
+
+    public String sayHello(String a) {
+        return "Hello " + a + ", Welcome to CXF RS Spring Boot World!!!";
+    }
+    
+}
