672119b27820628285f5e750bb62de8d968a73b2
Steve <stnguyen@redhat.com>
2017-07-31 17:47:23 +0800

    rest api

--- fuse-openshift/fis-script.sh
+++ fuse-openshift/fis-script.sh
@@ -1,4 +1,4 @@
 oc delete project demo
 oc new-project demo
 oc policy add-role-to-user admin developer -n demo
-oc new-app -f fis-quickstart-template.json -p GIT_REPO=https://github.com/sonngx/myapp.git -p BUILDER_VERSION=2.0-9 -n demo
+oc new-app -f s2i-spring-boot-camel-xml.json -p GIT_REPO=https://github.com/sonngx/myapp.git -p BUILDER_VERSION=2.0-9 -n demo
